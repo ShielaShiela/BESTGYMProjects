b4da78fac9e7d048f8a68cdcedca68d395bd96b2
dhika-m <M11302823@mail.ntust.edu.tw>
2026-02-28 16:26:10 +0800

    push latest changes

---
+++ PoseA/PoseA/MainView/AnalysisModeLeft/Views/Chart3DAnimationView.swift
@@ -0,0 +1,23 @@
+//
+//  3DChartAnimationView.swift
+//  PoseA
+//
+//  Created by Ardhika Maulidani on 12/12/25.
+//
+
+import SwiftUI
+
+struct Iris: Identifiable {
+    let id: Int
+    let sepalLength: Double
+    let sepalWidth: Double
+    let petalLength: Double
+    let petalWidth: Double
+    let species: String
+}
+
+struct 3DChartAnimationView: View {
+    var body: some View {
+        
+    }
+}
